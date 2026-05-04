aaae7ea12ed18c6371c58e10432c15809845d973
AntiFreeze81 <wnsgud3590@naver.com>
2026-05-04 10:51:32 +0900

    STEP 2

--- SpartaHW2.cpp
+++ SpartaHW2.cpp
@@ -28,13 +28,30 @@ int main() {
 	const int SIZE = 4;
 	int stat[SIZE] = { 0 }; //스탯 순서 HP, MP, 공격력, 방어력
 
-	//1-3, 1-4 cin으로 스탯 2개씩 입력받기
-	cout << "HP와 MP를 입력해주세요: ";
-	cin >> stat[0] >> stat[1];
-	cout << "공격력과 방어력을 입력해주세요: ";
-	cin >> stat[2] >> stat[3];
-	cout << '\n';
+	//1-3, 1-4 cin으로 스탯 2개씩 입력받기, 2-1, 2-2
+	while (true) {
+		cout << "HP와 MP를 입력해주세요: ";
+		cin >> stat[0] >> stat[1];
+		if (stat[0] > 50 && stat[1] > 50) {
+			break;
+		}
+		else {
+			cout << "HP나 MP의 값이 너무 작습니다. 다시 입력해주세요." << endl;
+		}
+	}
 
+	//2-3, 2-4
+	while(true){
+		cout << "공격력과 방어력을 입력해주세요: ";
+		cin >> stat[2] >> stat[3]; 
+		if (stat[2] > 50 && stat[3] > 50) {
+			break;
+		}
+		else {
+			cout << "공격력이나 방어력이 너무 작습니다. 다시 입력해주세요." << endl;
+		}
+	}
+	cout << '\n';
 
 	//1-5 printStatsu 함수 작성 후 호출
 	printStatus(name, stat);
